cd2649b5220c69e8a91db6bfe4941599a5cd67b7
austin <galaky2005@gmail.com>
2022-10-28 19:49:24 -0400

    added algorithm notes for iterative collapse;

--- wave-function-collapse-api/src/wave_function.rs
+++ wave-function-collapse-api/src/wave_function.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
 use serde::Deserialize;
-
+use rand::{self, seq::SliceRandom, SeedableRng};
 
 #[derive(Debug, Deserialize)]
 pub struct NodeState {
@@ -12,11 +12,50 @@ pub struct NodeState {
 pub struct Node {
     id: String,
     neighbor_node_ids: Vec<String>,
-    valid_node_states: Vec<String>
+    valid_node_state_ids: Vec<String>
+}
+
+struct CollapsableNode<'a> {
+    id: &'a str,
+    neighbor_node_ids: Vec<&'a str>,
+    possible_state_ids: Vec<&'a str>,
+    state_id_index: Option<u32>,
+    informed_neighbors: bool
+}
+
+impl<'a> CollapsableNode<'a> {
+    fn new(node: &'a Node) -> CollapsableNode {
+        let mut neighbor_node_ids: Vec<&str> = Vec::new();
+        for neighbor_node_id in node.neighbor_node_ids.iter() {
+            neighbor_node_ids.push(&neighbor_node_id);
+        }
+        let mut possible_state_ids: Vec<&str> = Vec::new();
+        for valid_node_state_id in node.valid_node_state_ids.iter() {
+            possible_state_ids.push(&valid_node_state_id);
+        }
+        CollapsableNode {
+            id: &node.id,
+            neighbor_node_ids: neighbor_node_ids,
+            possible_state_ids: possible_state_ids,
+            state_id_index: Option::None,
+            informed_neighbors: bool
+        }
+    }
+    fn randomize(&mut self, seed: u64) {
+        let mut random_instance = rand::rngs::StdRng::seed_from_u64(seed);
+        self.neighbor_node_ids.shuffle(&mut random_instance);
+        self.possible_state_ids.shuffle(&mut random_instance);
+    }
+    fn get_possible_states_count(&self) -> u32 {
+        let possible_states_count: u32 = self.possible_state_ids.len().try_into().expect("The length of self.possible_state_ids should be convertable to a u32.");
+        if let Some(index) = self.state_id_index {
+            
+        }
+    }
 }
 
 pub struct CollapsedWaveFunction {
-
+    // TODO fill with final nodes and their state
 }
 
 pub struct WaveFunction {
@@ -95,6 +134,32 @@ impl WaveFunction {
         return error_message;
     }
     pub fn collapse(&self) -> Result<CollapsedWaveFunction, String> {
+        let mut collapsable_nodes: Vec<CollapsableNode> = Vec::new();
+        for node in self.nodes.iter() {
+            let collapsable_node = CollapsableNode::new(node);
+            collapsable_nodes.push(collapsable_node);
+        }
+
+        // set sort necessary
+        // while the collapsable node index is less than the total number of collapsable nodes
+
+        // sort by (1) is_some() state_id_index and then (2) least possible states being first
+
+        // try to increment the current collapsable node state id index (maybe just going from None to Some(0))
+
+        // if failed to increment (then we need to try a different state for the most recent parent)
+
+        //      set state id index to None
+        //      decrement current collapsale node index
+        //      set sort unnecessary
+
+        // else
+
+        //      increment current collapsable node index
+        //      set sort necessary
+
+        
+
         Err(String::from("Not Implemented"))
     }
 }
