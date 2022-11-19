4ec767d20acf7bd3838a4e7946b0ba1d387b38c9
unknown <galaky2005@gmail.com>
2022-11-19 00:21:05 -0500

    fixed direct same-domain relations to no longer include other states to suggest invalid scenarios (ex: green comes after ivory, so red should not suggest that green could come after it); need to fix cross-domain overly-restricted logic;

--- wave-function-collapse/examples/zebra_puzzle.rs
+++ wave-function-collapse/examples/zebra_puzzle.rs
@@ -610,9 +610,34 @@ impl ZebraPuzzle {
 
                                                 // TODO account for when pointing from house 0 to house 1 that, when red, do not permit green since green is to the right of ivory
                                                 for looping_from_node_state_id in from_information_type.get_node_state_ids().iter() {
-                                                    for looping_to_node_state_id in to_information_type.get_node_state_ids().iter() {
-                                                        if looping_from_node_state_id != looping_to_node_state_id {
+                                                    if looping_from_node_state_id != &from_node_state_id {
+                                                        // this dependency does not exist between these two nodes and should therefore be restricted
+                                                        if !restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.contains_key(from_node_id) {
+                                                            restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.insert(from_node_id, HashMap::new());
                                                         }
+                                                        if !restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get(from_node_id).unwrap().contains_key(to_node_id) {
+                                                            restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get_mut(from_node_id).unwrap().insert(to_node_id, HashMap::new());
+                                                        }
+                                                        if !restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get(from_node_id).unwrap().get(to_node_id).unwrap().contains_key(looping_from_node_state_id) {
+                                                            restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get_mut(from_node_id).unwrap().get_mut(to_node_id).unwrap().insert(looping_from_node_state_id.clone(), Vec::new());
+                                                        }
+                                                        restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get_mut(from_node_id).unwrap().get_mut(to_node_id).unwrap().get_mut(looping_from_node_state_id).unwrap().push(to_node_state_id.clone());
+                                                    }
+                                                }
+
+                                                for looping_to_node_state_id in to_information_type.get_node_state_ids().iter() {
+                                                    if looping_to_node_state_id != &to_node_state_id {
+                                                        // this dependency does not exist between these two nodes and should therefore be restricted
+                                                        if !restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.contains_key(from_node_id) {
+                                                            restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.insert(from_node_id, HashMap::new());
+                                                        }
+                                                        if !restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get(from_node_id).unwrap().contains_key(to_node_id) {
+                                                            restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get_mut(from_node_id).unwrap().insert(to_node_id, HashMap::new());
+                                                        }
+                                                        if !restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get(from_node_id).unwrap().get(to_node_id).unwrap().contains_key(&from_node_state_id) {
+                                                            restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get_mut(from_node_id).unwrap().get_mut(to_node_id).unwrap().insert(from_node_state_id.clone(), Vec::new());
+                                                        }
+                                                        restricted_to_node_state_ids_per_from_node_state_id_per_to_node_id_per_from_node_id.get_mut(from_node_id).unwrap().get_mut(to_node_id).unwrap().get_mut(&from_node_state_id).unwrap().push(looping_to_node_state_id.clone());
                                                     }
                                                 }
                                             }
